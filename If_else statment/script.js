33cdea14fe3c0695be05da139af489072d1408a7
YaminiVeerabomma <yaminiveerabomma@gmail.com>
2026-02-03 23:06:15 +0530

    practice more about varibles ,statment

---
+++ If_else statment/script.js
@@ -0,0 +1,18 @@
+let age = 19;
+if (age >= 18) {
+  console.log("Audlt");
+} else {
+  console.log("minor");
+}
+let marks = 42;
+if (marks >= 40) {
+  console.log("pass");
+} else {
+  console.log("fail");
+}
+let isLoggedIn = false;
+if (isLoggedIn) {
+  console.log("Welcome back!");
+} else {
+  console.log("Please log in.");
+}
---
+++ Variable/script.js
@@ -0,0 +1,7 @@
+let name = "Yamini Veerabomma";
+let age = 24;
+let city = "Hyderabad";
+console.log(name);
+console.log(age);
+console.log(city);
+console.log(`${name} is ${age} years old and lives in ${city}.`);
